773fd27530d7cfb85e405ff276fa674326721358
Jakub Mandra <j.mandra@mdbootstrap.com>
2018-12-08 16:59:21 +0100

    Add debouonce and axios

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import infiniteScroll from 'vue-infinite-scroll';
 
 Vue.config.productionTip = false
+Vue.use(infiniteScroll);
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
