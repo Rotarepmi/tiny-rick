79eb3e90dbdcd58e9ed2ffb1c23ba21d9ee3c8e0
Jakub Mandra <j.mandra@mdbootstrap.com>
2018-12-09 12:58:49 +0100

    Build reusable  BaseItem component

---
+++ src/components/BaseItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="base-item">
+    <header class="base-item__header">
+      <img v-if="image" :src="image" class="base-item__img">
+      <div class="base-item__header-content">
+        <h3 class="base-item__title">{{ name }}</h3>
+        <span class="base-item__subtitle">{{ subtitle }}</span>
+      </div>
+    </header>
+
+    <p v-if="content" class="base-item__content">{{ content }}}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  props: {
+    image: {
+      type: String,
+      default: ""
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    subtitle: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
