2efe095d91c96ff7d8cc8898061e6b204fb3d467
Jakub Mandra <j.mandra@mdbootstrap.com>
2018-12-09 12:58:11 +0100

    Prepare routing

--- src/router.js
+++ src/router.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import TheEpisodesView from '@/views/episodes/TheEpisodesView';
+import BaseEpisodeView from '@/views/episodes/BaseEpisodeView';
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-    }
-  ]
-})
+    { path: '/', component: TheEpisodesView },
+    { path: '/episode/:id', component: BaseEpisodeView }
+  ],
+  mode: 'history'
+});
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -2,22 +2,17 @@
   <div>
     <the-header/>
     <main class="main">
-      <the-episodes-view/>
-      <!-- <base-episode-view/> -->
+      <router-view/>
     </main>
   </div>
 </template>
 
 <script>
 import TheHeader from "@/components/layout/TheHeader";
-import TheEpisodesView from "./episodes/TheEpisodesView";
-import BaseEpisodeView from "./episodes/BaseEpisodeView";
 
 export default {
   components: {
-    TheHeader,
-    TheEpisodesView,
-    BaseEpisodeView
+    TheHeader
   }
 };
 </script>
